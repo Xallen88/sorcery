da471767edfd9571dc30d2981508df02dd4d9947
StevensyKim <Steven.sy.kim@gmail.com>
2017-07-13 17:37:24 -0400

    Update Minion.h

--- files/Minion.h
+++ files/Minion.h
@@ -2,6 +2,16 @@
 #define MINION_H
 
 
+Class Minion {
+  int atk;
+  int def;
+ public:
+  Minion(int a, int d);
+  ~Minion() override;
+  void Play() override;
+  void Activate() override;
+  void Activate(Card *target) override;
+};
 
 
 
