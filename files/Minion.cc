f97b7470b9a81906f65187310bcaa3648d158850
StevensyKim <Steven.sy.kim@gmail.com>
2017-07-13 17:54:40 -0400

    Update Minion.cc

--- files/Minion.cc
+++ files/Minion.cc
@@ -3,3 +3,12 @@ using namespace std;
 
 Minion::Minion(int a, int d): atk{a}, def{d}{}
 void Minion::Attack(Player *p) { /*Check if can attack then */ p->decrementLife(atk); }
+void Minion::Attack(Minion *m) { /*Check if can attack then */ m->decrementLife(atk); }
+void Minion::decrementLife(int i) { 
+ hp -= i;
+ if (hp < 0) hp = 0;
+}
+void Minion::incrementLife(int i) {
+ hp += i;
+ if (hp > maxHp) hp = maxHp;
+}
