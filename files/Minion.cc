59829fe8ad485382368da3aa53a14669c3d86153
StevensyKim <Steven.sy.kim@gmail.com>
2017-07-13 18:01:32 -0400

    Update Minion.cc

--- files/Minion.cc
+++ files/Minion.cc
@@ -1,7 +1,7 @@
  #include "Minion.h"
 using namespace std;
 
-Minion::Minion(int a, int d): atk{a}, def{d}{}
+Minion::Minion(int a, int h): baseAtk{a}, baseHp{h}, curAtk{a}, curHp{h}, maxHp{h}{}
 void Minion::Attack(Player *p) { /*Check if can attack then */ p->decrementLife(atk); }
 void Minion::Attack(Minion *m) { /*Check if can attack then */ m->decrementLife(atk); }
 void Minion::decrementLife(int i) { 
