7e34f12f24032a520372dba7129a0f986c1dffc2
StevensyKim <Steven.sy.kim@gmail.com>
2017-07-16 19:31:45 -0400

    Update Minion.cc

--- files/Minion.cc
+++ files/Minion.cc
@@ -1,7 +1,8 @@
  #include "Minion.h"
 using namespace std;
 
-Minion::Minion(int a, int h): baseAtk{a}, baseHp{h}, curAtk{a}, curHp{h}, maxHp{h}{}
+Minion::Minion(string name, int a, int h): Card{name}, curAtk{a}, curHp{h},
+                                                       maxAtk{a}, maxHp{h}{}
 void Minion::Attack(Player *p) { /*Check if can attack then */ p->decrementLife(atk); }
 void Minion::Attack(Minion *m) { /*Check if can attack then */ m->decrementLife(atk); }
 void Minion::decrementLife(int i) { 
@@ -11,4 +12,19 @@ void Minion::decrementLife(int i) {
 void Minion::incrementLife(int i) {
  hp += i;
  if (hp > maxHp) hp = maxHp;
+}
+bool Minion::isDead {
+ return hp <= 0;
+}
+void Minion::addEnchant(Enchantments *e) {
+  enchantments.emplace_back(e);
+  numEnch++;
+  //Add the stats in from enchantments
+}
+void Minion::clearAllEnchants() {
+  for (int k = 0; k < numEnch; k++) 
+    enchantments.popback()
+  }
+  numEnch = 0;
+  //Take away stats from echantments
 }
