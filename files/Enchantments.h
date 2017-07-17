c0d5bbb82f51ad77eed0632875a5d6756f9a8fbf
StevensyKim <Steven.sy.kim@gmail.com>
2017-07-16 20:00:45 -0400

    Update Enchantments.h

--- files/Enchantments.h
+++ files/Enchantments.h
@@ -1,1 +1,24 @@
+#ifndef _ENCHANTMENTS_H_
+#define _ENCHANTMENTS_H_
+#include "Card.h"
+#include <string>
 
+using namespace std;
+
+
+
+class Enchantment : public Card {
+  int atkVal;
+  int hpVal;
+  char op;
+ public:
+  Enchantment(string name, int atkVal, int hpVal, char op);
+  ~Enchantment();
+  void Activate(Card *c) override;
+  int getAVal();
+  int getHVal();
+  char getOp();
+
+
+
+#endif
