45eabaa830217f23dacff3420b240fd912d98e3d
StevensyKim <Steven.sy.kim@gmail.com>
2017-07-24 01:43:41 -0400

    Update Enchantments.cc

--- files/Enchantments.cc
+++ files/Enchantments.cc
@@ -15,11 +15,13 @@ Enchantment::Enchantment(string name) : Card(name){
   stringstream ss;
   string line;
   string fileName = name;
-  //Defaulting in the case that it is not a normalEnch
+  //Defaulting in the case that it is not a normal enchant
   atkVal = 0;
   hpVal = 0;
+  costVal = 0;
   atkOp = '+';
   hpOp = '+';
+  costOp = '+';
 
   int len = fileName.length();
   for (int i = 0; i < len; ++i) {
@@ -30,33 +32,35 @@ Enchantment::Enchantment(string name) : Card(name){
     }
   }
 
-  fileName = "minions/" + fileName + ".info";
+  fileName = "enchantments/" + fileName + ".info";
   ifstream infoFile (fileName);
   getline(infoFile, line);
   ss.str(line);
   //Adding cost, atk, hp
   ss >> cost;
-  ss >> normalEnch;
-  if (normalEnch == 0) {
-    ss >> atkVal >> hpVal >> atkOp >> hpOp; 
+  ss >> enchType;
+  if (enchType == 0) {
+    ss >> atkVal >> hpVal >> atkOp >> hpOp;
   } else {//Only not normal Enchants have desc
-    getline(ss, description);	
+    if (enchType == 1) {//Changing ability cost
+      ss >> costVal >> costOp;
+    }
+    getline(ss, description);
     // feed remaining line into description
   }
 }
 Enchantment::~Enchantment() {}
 int Enchantment::getAVal() { return atkVal; }
 int Enchantment::getHVal() { return hpVal; }
+int Enchantment::getCVal() { return costVal; }
 char Enchantment::getAOp() { return atkOp; }
 char Enchantment::getHOp() { return hpOp; }
+char Enchantment::getCOp() { return costOp; }
+int Enchantment::getEnchType() { return enchType; }
 void Enchantment::Activate(Card *c) { 
-  //Making sure it is a Minion to enchant  
-  if (type != "Minion") {
-     //Display error message for using enchantment on a non-minion
-     return;
-  }
-  //c->addEnchant(this);//not sure if this is * or & or none
-  //Doing enchantment stats in the minion class
+  if (name == "Silence") SilenceEnchantment(c);
+  Minion *m = (Minion *) c;
+  m->addEnchant(this);
 }
                          
 void Enchantment::Play(){
@@ -64,9 +68,8 @@ void Enchantment::Play(){
 }   
 
 void Enchantment::Play(Card* c){
-  if (c->getType() != "Minion") return;
-  Minion *m = (Minion *) c;
-  m->addEnchant(this);
+  if (c->getType() != "Minion") return;//Checking if minion
+  Activate(c);
 }    
 
 void Enchantment::Activate(){
